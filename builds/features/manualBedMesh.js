c3799f39589dfa0321926d267d47b98a5b9bfcf9
sergeyhatunzev <153432330+sergeyhatunzev@users.noreply.github.com>
2024-10-26 22:32:52 +0500

    Update manualBedMesh.js

--- builds/features/manualBedMesh.js
+++ builds/features/manualBedMesh.js
@@ -3,7 +3,6 @@ module.exports = {
     configuration: {
         enable: [
             "PROBE_MANUALLY",
-            
             "MESH_BED_LEVELING",
             "RESTORE_LEVELING_AFTER_G28",
             "LCD_BED_LEVELING",
