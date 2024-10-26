55081324480205c201fb0582ce091920c9aee953
sergeyhatunzev <sergeyhatunzev@gmail.com>
2024-10-26 23:10:13 +0500

    Возврат к истокам

---
+++ builds/default.js
@@ -0,0 +1,14 @@
+module.exports = {
+    board_env: "STM32F103RE_creality",
+    min_version: "2.1.2",
+    meta: {
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
+    },
+    based_on: {
+        repo: "https://github.com/MarlinFirmware/Configurations/",
+        path: "/config/examples/Creality/Ender-3/CrealityV422/",
+        stable_branch: "release-{{marlin_version}}",
+        nightly_branch: "bugfix-2.1.x"
+    }
+};
