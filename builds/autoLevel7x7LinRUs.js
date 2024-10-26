593544d93d8ebf510abbd773676021131a4429c0
sergeyhatunzev <153432330+sergeyhatunzev@users.noreply.github.com>
2024-10-26 17:18:37 +0500

    Update autoLevel7x7LinRUs.js

--- builds/autoLevel7x7LinRUs.js
+++ builds/autoLevel7x7LinRUs.js
@@ -6,10 +6,15 @@ module.exports = {
         nightly_name: "ender_3_4.2.2-{{current_date}}-auto_level_7x7_lin-{{uid}}"
     },
     configuration: {
+        disable: [
+            "GRID_MAX_POINTS_X",
+            "DISPLAY_CHARSET_HD44780",
+            "LCD_LANGUAGE"
+        ],
         enable: [
             ["GRID_MAX_POINTS_X", 7],
-            ["DISPLAY_CHARSET_HD44780", "CYRILLIC" ],
-            ["LCD_LANGUAGE", ru ]
+            ["DISPLAY_CHARSET_HD44780", "CYRILLIC"],
+            ["LCD_LANGUAGE", "ru"] // оставляем "ru" как строку в скрипте
         ]
     }
 };
