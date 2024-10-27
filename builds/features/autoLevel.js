299e86f4e7c58c694321cd7632252a62ca81cf27
sergeyhatunzev <sergeyhatunzev@gmail.com>
2024-10-27 18:19:34 +0500

    ╨Ю╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡ ╤А╨╡╨┐╨╛╨╖╨╕╤В╨╛╤А╨╕╤П

--- builds/features/autoLevel.js
+++ builds/features/autoLevel.js
@@ -27,9 +27,9 @@ module.exports = {
 
             //symmetrical
             ["PROBING_MARGIN_LEFT", 70],
-            ["PROBING_MARGIN_RIGHT", 15],
-            ["PROBING_MARGIN_FRONT", 70],
-            ["PROBING_MARGIN_BACK", 15]
+            ["PROBING_MARGIN_RIGHT", 5],
+            ["PROBING_MARGIN_FRONT", 5],
+            ["PROBING_MARGIN_BACK", 5]
         ]
     }
 };
