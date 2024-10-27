078c4f692958060b3af5633038ef4670a24832a7
sergeyhatunzev <sergeyhatunzev@gmail.com>
2024-10-27 12:22:00 +0500

    ╨Ю╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡ ╤А╨╡╨┐╨╛╨╖╨╕╤В╨╛╤А╨╕╤П

--- builds/autoLevel5x5_Russian _PROBING_MARGIN_50.js
+++ builds/autoLevel5x5_Russian _PROBING_MARGIN_50.js
@@ -2,13 +2,13 @@ module.exports = {
     extends: "builds/base.js",
     include: "builds/features/autoLevel.js",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-auto_level_5x5_PROBING_MARGIN_20-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-auto_level_5x5_PROBING_MARGIN_20-{{uid}}"
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-auto_level_5x5_PROBING_MARGIN_50-{{uid}}",
+        nightly_name: "ender_3_4.2.2-{{current_date}}-auto_level_5x5_PROBING_MARGIN_50-{{uid}}"
     },
     configuration: {
         enable: [
             ["GRID_MAX_POINTS_X", 5],
-			["PROBING_MARGIN", 20]
+			["PROBING_MARGIN", 50]
         ]
     }
 };
